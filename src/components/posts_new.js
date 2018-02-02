9b8bc4bee2233d793d84c72983f54aba3a672d91
xdays <easedays@gmail.com>
2018-02-02 18:27:14 +0800

    add blog app

---
+++ src/components/posts_new.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
+class PostNew extends Component {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+    return (
+      <div className={className}>
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit(values) {
+    // this === component
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <div>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+          <Field
+            label="Post Title"
+            name="title"
+            component={this.renderField}
+          />
+          <Field
+            label="Categories"
+            name="categories"
+            component={this.renderField}
+          />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField}
+          />
+          <button type="submit" className="btn btn-primary">Submit</button>
+          <Link to="/" className="btn btn-danger">Cancel</Link>
+        </form>
+      </div>
+    );
+  }
+}
+
+function validate(values) {
+  // console.log(values)
+  const errors = {};
+
+  // validate the inputs
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter som categories";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
+
+  // if errors is empty, the form is fine to submit
+  // or redux form assumes form is invalid
+  return errors
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostNewForm'
+})(
+  connect(null, { createPost })(PostNew)
+);
---
+++ src/components/posts_show.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from '../actions';
+
+class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
+  onDelectClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Load...</div>;
+    }
+    return (
+      <div>
+        <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDelectClick.bind(this)}
+        >Delete Post</button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
